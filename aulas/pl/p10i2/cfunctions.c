16c586bb35c936573043e3af3ef11dc2c27860bd
Francisco Pereira <fsp@utad.pt>
2021-12-17 04:26:23 +0000

    Aula T2-S05 e T2-S06

---
+++ aulas/pl/p10i2/cfunctions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//char text[]="This is C calling...";
+
+int cadd(int a_r0, int b_r1)
+{	
+  return a_r0 + b_r1;
+}
+
+void cfunction()
+{
+	puts("This is the C code...");
+//  printf("This is the C code...");
+}
