5164bf13ca6fd3d8632a208c04b53cb9837a97e1
Francisco Pereira <fsp@utad.pt>
2022-06-02 08:31:57 +0100

    minor updates

--- aulas/t/SCO-T2-S07-S08/prog5fp.c
+++ aulas/t/SCO-T2-S07-S08/prog5fp.c
@@ -12,9 +12,9 @@ int main()
   int a = 10, b = 20;
   int (*function_ptr)(int, int) = &cadd;
 
-  printf("Calling cfunction_asm with pointer to cadd(), a=%d, b=%d\n", a, b);
+  printf("C: Calling cfunction_asm with pointer to cadd(), a=%d, b=%d\n", a, b);
 
-  printf("Result of %d + %d via cfunction_asm->cadd() is %d\n", a, b, cfunction_asm(a, b, function_ptr));
+  printf("C: Result of %d + %d via cfunction_asm->cadd() is %d\n", a, b, cfunction_asm(a, b, function_ptr));
 
   return 0;
 }
